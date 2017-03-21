71205e998e8c38ffe6d03cfec70b234ddffb847e
RaulCM <raulcanomontero@hotmail.com>
2017-03-21 12:21:27 +0100

    Realizada la calculadora sobre Django

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from calc import views
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(\d+)\+(\d+)$', views.suma, name="suma"),
+    url(r'^(\d+)\-(\d+)$', views.resta, name="resta"),
+    url(r'^(\d+)\*(\d+)$', views.multiplicacion, name="multiplicacion"),
+    url(r'^(\d+)/(\d+)$', views.division, name="division"),
+	#url(r'.*'', views.error, name="error"),
 )
