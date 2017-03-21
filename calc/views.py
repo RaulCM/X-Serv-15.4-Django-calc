f85a0b475d03503ef10e64650d62525484744313
Raúl Cano <raulcanomontero@hotmail.com>
2017-03-21 18:54:29 +0100

    Update views.py

--- calc/views.py
+++ calc/views.py
@@ -22,7 +22,3 @@ def multiplicacion(request, op1, op2):
 def division(request, op1, op2):
     return HttpResponse("<h1>" + op1 + "/" + op2 + "=" +
                         str(int(op1) / int(op2)) + "</h1>")
-
-
-def error(request, parametro):
-    return HttpResponse("<h1>Not Found</h1>")
